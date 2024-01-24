308efb36b8628b686e7430e8d636a533bd78b1a6
EkaTur <savosya23@gmail.com>
2024-01-24 20:53:04 +0100

    changed photos

--- src/dataTwo.js
+++ src/dataTwo.js
@@ -1,21 +1,24 @@
 import floristThree from './Assets/floristThree.jpg';
+import florist from './Assets/florist.jpg';
+import floristTwo from './Assets/floristTwo.jpg';
+import floristFour from './Assets/floristFour.jpg';
 
 export const dataTwo = [
     {
         id: 1,
-        image: 'https://img.freepik.com/free-photo/woman-making-beautiful-floral-arrangement_23-2148846514.jpg?w=1060&t=st=1705939945~exp=1705940545~hmac=c433c57d3f0cd18e792bf946157680514ac144932e3760e6ed26137a95d0c18b',
+        image: florist,
         description: "We are a small family-owned flower business, where every blossom is the result of our dedication to quality and uniqueness."
     },
 
     {
         id: 2,
-        image: "https://img.freepik.com/free-photo/medium-shot-people-celebrating-mother-s-day_23-2151207223.jpg?w=1060&t=st=1705941899~exp=1705942499~hmac=8d14a2226bbf4a0914784c29ec6983442b70752ef211b3f729c95ee770b7e035",
+        image: floristTwo,
         description: "In our garden, we cultivate flowers with special attention to detail. Our passion for plants and our desire to provide you with the freshest and most exquisite blooms make our arrangements truly distinctive. "
     },
 
     {
         id: 3,
-        image: "https://img.freepik.com/free-photo/high-angle-view-male-florist-tying-flower-bouquet_23-2148075302.jpg?w=1060&t=st=1705942188~exp=1705942788~hmac=ffffbe8045d1f3bd9a49ef80fcdcce4ed362221fa952584d6e394e909cfbf3fa",
+        image: floristFour,
         description: "The entire process, from seeding to bouquet arrangement, is carried out within our family circle, ensuring that each flower brings joy and warmth."
     },
 
